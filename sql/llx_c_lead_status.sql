ffc29508e81618eda394547f7a769dbaa2d3d5df
Florian Henry <florian.henry@open-concept.pro>
2014-02-24 15:23:57 +0100

    fix size

--- sql/llx_c_lead_status.sql
+++ sql/llx_c_lead_status.sql
@@ -17,7 +17,7 @@
 create table llx_c_lead_status
 (
   rowid       integer AUTO_INCREMENT PRIMARY KEY,
-  code 		varchar(5) UNIQUE NOT NULL,
+  code 		varchar(10) UNIQUE NOT NULL,
   label         varchar(50),
   active      tinyint DEFAULT 1 NOT NULL
 )ENGINE=innodb;
