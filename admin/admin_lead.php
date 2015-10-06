f04571e65259906db752331c6a75c49ffe7339a2
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2015-10-06 11:11:54 +0200

    [Qual] Wrong object type

--- admin/admin_lead.php
+++ admin/admin_lead.php
@@ -225,7 +225,7 @@ foreach ($dirmodels as $reldir) {
 						// Info
 						$htmltooltip = '';
 						$htmltooltip .= '' . $langs->trans("Version") . ': <b>' . $module->getVersion() . '</b><br>';
-						$nextval = $module->getNextValue($user->id, $mysoc, $propal);
+						$nextval = $module->getNextValue($user->id, $mysoc, $businesscase);
 						if ("$nextval" != $langs->trans("NotAvailable")) 						// Keep " on nextval
 						{
 							$htmltooltip .= '' . $langs->trans("NextValue") . ': ';
