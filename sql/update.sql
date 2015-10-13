8a609ba85ad051af4be424b3ccb33aacc6094549
Florian HENRY <florian.henry@open-concept.pro>
2015-10-13 16:16:13 +0200

    dolibarr 3.8

--- sql/update.sql
+++ sql/update.sql
@@ -1,4 +1,4 @@
 ALTER TABLE llx_lead ADD COLUMN note_public text AFTER description;
 ALTER TABLE llx_lead ADD COLUMN note_private text AFTER note_public;
-ALTER TABLE llx_lead ADD COLUMN position integer AFTER label;
-ALTER TABLE llx_lead ADD COLUMN percent double(5,2) AFTER position;
+ALTER TABLE llx_c_lead_status ADD COLUMN position integer AFTER label;
+ALTER TABLE llx_c_lead_status ADD COLUMN percent double(5,2) AFTER position;
